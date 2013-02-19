3dbde5de4161c250b485cb77d3cc6d0e2e8ebc84
Chad Whitacre <chad@zetaweb.com>
2013-02-19 12:01:00 -0500

    Add reset button

--- coin_counter.ino
+++ coin_counter.ino
@@ -5,7 +5,29 @@
 
 
 Adafruit_7segment counter = Adafruit_7segment();
+OneButton resetButton(11, true);
 OneButton incrementSwitch(3, true);
+int count;
+
+// Logic
+
+void reset()
+{
+  count = 0;
+  counter.print(0, DEC);        // turns everything off
+  counter.writeDigitNum(4, 0);  // writes a zero on the right
+  counter.writeDisplay();       // == flush
+}
+
+void increment()
+{
+  count = count == 9999 ? 0 : count + 1;
+  counter.print(count, DEC);
+  counter.writeDisplay();
+}
+
+
+// Arduino Hooks
 
 void setup()
 {
@@ -16,25 +38,14 @@ void setup()
   
   incrementSwitch.setClickTicks(10);
   incrementSwitch.attachClick(increment);
-}
-
-int count = 0;
-
-void reset()
-{
-  counter.writeDigitNum(4, 0);
-  counter.writeDisplay();
-}
-
-void increment()
-{
-  count = count == 9999 ? 0 : count + 1;
-  counter.print(count, DEC);
-  counter.writeDisplay();
+  
+  resetButton.setPressTicks(10);
+  resetButton.attachPress(reset);
 }
 
 void loop()
 {
+  resetButton.tick();
   incrementSwitch.tick();
   delay(1);
 }
